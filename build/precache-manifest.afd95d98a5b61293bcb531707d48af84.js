a8662114d6f4172ae7fc2dd8880d8c0e80a9e702
vladimir9255 <69668555+vladimir9255@users.noreply.github.com>
2020-12-25 07:19:42 +0900

    sd

--- build/precache-manifest.afd95d98a5b61293bcb531707d48af84.js
+++ build/precache-manifest.afd95d98a5b61293bcb531707d48af84.js
@@ -12,8 +12,8 @@ self.__precacheManifest = [
     "url": "/static/js/main.fc0df96f.chunk.js"
   },
   {
-    "revision": "f5a8bf4ec4640e9850b4",
-    "url": "/static/js/runtime~main.1cca03c1.js"
+    "revision": "1f950a183eecd6e8320f",
+    "url": "/static/js/runtime~main.25d3b1b1.js"
   },
   {
     "revision": "8e76f6083d0d7bbec2c1",
@@ -156,8 +156,8 @@ self.__precacheManifest = [
     "url": "/static/js/37.8b81aa73.chunk.js"
   },
   {
-    "revision": "c2314e88c2849670825c",
-    "url": "/static/js/38.ba3ceb9e.chunk.js"
+    "revision": "a1a6b9717122f06dbc82",
+    "url": "/static/js/38.fdc37c09.chunk.js"
   },
   {
     "revision": "c3ff0d5c0b928b48fdba",
@@ -204,7 +204,7 @@ self.__precacheManifest = [
     "url": "/static/media/sidebar-4.80d4a4e5.jpg"
   },
   {
-    "revision": "830295e0fa4ec272fc71225f73d2a473",
+    "revision": "a5497a85deb2505beae76efae81795ad",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a76eb047b0b4d11b4134187106393e4.js"
+  "/precache-manifest.afd95d98a5b61293bcb531707d48af84.js"
 );
 
 workbox.clientsClaim();
